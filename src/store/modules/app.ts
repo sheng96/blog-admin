a16429b7323562db8eb8e391df41434934f23fad
sheng <407497818@qq.com>
2022-10-25 22:48:10 +0800

    引入pinia

---
+++ src/store/modules/app.ts
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+// defineStore 调用后返回一个函数，调用该函数获得 Store 实体
+export const userMenuStore = defineStore('app', {
+  // state：返回对象的函数
+  state: () => {
+    return {
+      collapsed: false //侧边菜单的开关状态
+    }
+  }
+})
