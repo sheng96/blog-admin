ad70336a91d3c981fef3721be55432161f97977f
sheng <407497818@qq.com>
2022-12-06 23:01:22 +0800

    增加vite环境变量提升

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  // 更多环境变量...
-  readonly VITE_BASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,12 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
