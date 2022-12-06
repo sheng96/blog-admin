cb83709fc027332ca1a04144fa5a6381d5e3f71d
sheng <407497818@qq.com>
2022-12-06 23:02:35 +0800

    增加基础类型定义

--- src/api/model/baseModel.ts
+++ src/api/model/baseModel.ts
@@ -7,3 +7,22 @@ export interface BasicFetchResult<T> {
   items: T[]
   total: number
 }
+
+export interface BaseModel<T> {
+  code: number
+  data: T
+  success: boolean
+}
+
+export interface PagesModel<K> {
+  data: K
+  page: number
+  pageTotal: number
+  total: number
+}
+export interface PagesBaseModel<T> {
+  code: number
+  data: PagesModel<T>
+  success: boolean
+}
+
